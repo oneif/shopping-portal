15445e03de50cd5816383b0e8e9a6d6d53cc0db3
if <if17825578404@126.com>
2024-02-01 21:30:39 +0800

    feat：first commit

--- components.d.ts
+++ components.d.ts
@@ -9,9 +9,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
-    ElDatePicker: (typeof import('element-plus/es'))['ElDatePicker']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
+    SvgIcon: typeof import('./src/components/SvgIcon.vue')['default']
   }
 }
